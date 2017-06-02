7e416aa6fbe5f786137f0fc4150fb7030a29f255
Sidney <snemzer@verizon.net>
2017-06-02 18:11:15 +0000

    Add material ui Theme

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import registerServiceWorker from './registerServiceWorker'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
 import './index.css'
+
+injectTapEventPlugin()
 
 function fetchPosts() {
   return new Promise((resolve, reject) => {
@@ -26,9 +31,11 @@ function fetchPosts() {
 }
 
 ReactDOM.render(
-  <App 
-    fetchPosts={fetchPosts}
-  />, 
+  <MuiThemeProvider>
+    <App
+      fetchPosts={fetchPosts}
+    />
+  </MuiThemeProvider>,
   document.getElementById('root')
 )
 registerServiceWorker()
