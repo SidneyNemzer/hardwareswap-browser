d1ebc843121bbe165bfde593462327f4cb88deef
SidneyNemzer <sidney@ucode.com>
2017-06-10 17:10:55 -0700

    Cleanup
    * Convert to functional component
    * Convert inline styles to JSS
    * Remove loading logic
    * Swap "Hidden" and "Saved" posts to reflect tab order

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,78 +1,50 @@
 import React, { Component } from 'react'
 import { withTheme } from 'material-ui/styles'
 import Statistic from './Statistic'
+import { withStyles, createStyleSheet } from 'material-ui/styles'
 
-class Header extends Component {
-  render() {
-    const { theme } = this.props
-
-    const headerStyle = {
-      backgroundColor: theme.palette.primary[500],
-      color: 'white',
-      padding: 20
-    }
-
-    const headingStyle = {
-      textAlign: 'center'
-    }
-
-    const infoBarStyle = {
-      textAlign: 'center',
-      display: 'flex',
-      justifyContent: 'center'
-    }
-
-    if (this.props.loading) {
-      return (
-        <header style={headerStyle}>
-          <h1 style={headingStyle}>
-            <a
-              href="https://reddit.com/r/hardwareswap"
-              target="_blank"
-              title="Open /r/hardwareswap in a new tab"
-              rel="noopener noreferrer"
-            >
-              /r/hardwareswap
-            </a>
-            &nbsp;Browser
-          </h1>
-          <div style={infoBarStyle}>
-            Loading...
-          </div>
-        </header>
-      )
-    }
-
-    return (
-      <header style={headerStyle}>
-        <h1 style={headingStyle}>
-          <a
-            href="https://reddit.com/r/hardwareswap"
-            target="_blank"
-            title="Open /r/hardwareswap in a new tab"
-            rel="noopener noreferrer"
-          >
-            /r/hardwareswap
-          </a>
-          &nbsp;Browser
-        </h1>
-        <div style={infoBarStyle}>
-          <Statistic
-            number={this.props.loadedPosts}
-            label="Loaded Posts"
-          />
-          <Statistic
-            number={this.props.hiddenPosts}
-            label="Hidden Posts"
-          />
-          <Statistic
-            number={this.props.savedPosts}
-            label="Saved Posts"
-          />
-        </div>
-      </header>
-    )
+const styleSheet = createStyleSheet('Header', theme => ({
+  header: {
+    backgroundColor: theme.palette.primary[500],
+    color: 'white',
+    padding: 20,
+    textAlign: 'center'
+  },
+  statsBar: {
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center'
   }
-}
+}))
 
-export default withTheme(Header)
+const Header = (props) => (
+  <header className={props.classes.header}>
+    <h1>
+      <a
+        href="https://reddit.com/r/hardwareswap"
+        target="_blank"
+        title="Open /r/hardwareswap in a new tab"
+        rel="noopener noreferrer"
+      >
+        /r/hardwareswap
+      </a>
+      &nbsp;Browser
+    </h1>
+    <div className={props.classes.statsBar}>
+      <Statistic
+        number={props.loadedPosts}
+        label="Loaded Posts"
+      />
+      <Statistic
+        number={props.savedPosts}
+        label="Saved Posts"
+      />
+      <Statistic
+        number={props.hiddenPosts}
+        label="Hidden Posts"
+      />
+    </div>
+  </header>
+)
+
+export default withStyles(styleSheet)(Header)
